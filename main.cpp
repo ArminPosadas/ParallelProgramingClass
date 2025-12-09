47cbafe24f0f8f9f4a8e4f327b40a3d07ee90b90
ArminPosadas <132945408+ArminPosadas@users.noreply.github.com>
2025-12-09 09:50:20 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
 
 int main(){
-  std::cout << "Lol" << std::endl;
+  std::cout << "Said llego con un gato con mano amarilla de 4 dedos :)." << std::endl;
+  std::cout << "Lo que dijo Alan Albino es correcto, la cajetie con los espacio y guiones" << std::endl;
+  std::cout << "Christian dice Nice!!!" << std::endl;
+  std::cout << "Karol dijo hola en voz bajita" << std::endl;
+  std::cout << "Salvador dice lo basico y emmanuel dice que poco, \n\taunque sea basico lo poco"<< std::endl;
+  int b;
+  int c;
 
+  c=15;
+  b=4;
+  std::cout << c+b << std::endl;
+  
+// OTRO CODIO como de pruebas, actividades extras, etc.
   return 0;
 }
